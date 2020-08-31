811c85d0e2132b5430242216bc2d89a77d52562e
unknown <jueeli97@gmail.com>
2020-08-31 20:37:25 +0530

    For Loop
    Signed-off-by: unknown <jueeli97@gmail.com>

---
+++ index.js
@@ -0,0 +1,22 @@
+// LOOP
+
+// For Loop
+
+ let dataArray = [1,2,3,"abcd",5,6,7,true];
+
+ for (let counter = 0; counter <= dataArray.length - 1; counter++) {
+    console.log(dataArray[counter]);
+    
+ }
+
+
+ for(let value of dataArray){
+     console.log(value);
+ }// elements of array
+
+ for(let value in dataArray){
+     console.log(value);
+ }// index position of array
+ 
+
+ 
